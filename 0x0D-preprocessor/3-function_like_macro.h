76a6fc601666209700b48c24ee70144c60516781
victor kelvin <victorkelvin661@gmail.com>
2023-03-20 13:23:28 -0700

    C - Preprocessor project

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,21 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+
+
+=== 4-sum.h ===
+
+
+#ifndef _SUM_H_
+#define _SUM_H_
+
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif
